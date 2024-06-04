a8fa339d48ab4e371858455cc5aec8baa441470f
Кирилл Солдатенко <soldatenko.kirillas@gmail.com>
2024-06-04 13:25:46 +0300

    add .Net Identity with roles

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 using DataLayer.Model;
 using DataLayer.Model.EF;
+using EventSeller.DataLayer.Entities;
 using EventSeller.Services.Interfaces;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Services;
 using Services.Service;
@@ -17,15 +19,21 @@ builder.Services.AddControllers().AddJsonOptions(x =>
 });
 
 builder.Services.AddDbContext<SellerContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("SellerContextConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SellerContextConnection")
+    ,x => x.MigrationsAssembly("EventSeller.DataLayer")));
 builder.Services.AddScoped<IUnitOfWork,UnitOfWork>();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 RegisterServices(builder.Services);
+
+builder.Services.AddIdentity<User,IdentityRole>()
+                .AddEntityFrameworkStores<SellerContext>()
+                .AddDefaultTokenProviders();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+await SetupRolesAsync(app.Services);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -34,7 +42,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseAuthentication();
 app.UseAuthorization();
+
 
 app.MapControllers();
 
@@ -48,4 +58,19 @@ void RegisterServices(IServiceCollection services)
     services.AddScoped<IPlaceHallService, PlaceHallService>();
     services.AddScoped<ITicketSeatService, TicketSeatService>();
     services.AddScoped<ITicketService, TicketService>();
+}
+async Task SetupRolesAsync(IServiceProvider appService)
+{
+    using (var scope = appService.CreateScope())
+    {
+        var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+        var roles = new[] { "Basic", "Event manager", "Venue manager", "Admin", "Super admin" };
+        foreach (var role in roles)
+        {
+            if (!await roleManager.RoleExistsAsync(role))
+            {
+                await roleManager.CreateAsync(new IdentityRole(role));
+            }
+        }
+    }
 }
